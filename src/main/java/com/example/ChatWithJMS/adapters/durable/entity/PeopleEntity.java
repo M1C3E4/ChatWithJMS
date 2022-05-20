8ba71d0a92594fc0a4dc3c5da251af5a99ccf19d
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-05-20 14:42:55 +0100

    entityPeopleFinal

--- src/main/java/com/example/ChatWithJMS/adapters/durable/entity/PeopleEntity.java
+++ src/main/java/com/example/ChatWithJMS/adapters/durable/entity/PeopleEntity.java
@@ -1,4 +1,28 @@
 package com.example.ChatWithJMS.adapters.durable.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "people")
 public class PeopleEntity {
+
+    @Id
+    @Column(name = "people_id", nullable = false)
+    private UUID peopleId;
+    @Column(name = "people_name", nullable = false, unique = true)
+    private String peopleName;
+    @Column(nullable = false)
+    private Timestamp doTime;
+    @ManyToMany(mappedBy = "ductPeoples")
+    //private List<> ductEntityList;
+
 }
