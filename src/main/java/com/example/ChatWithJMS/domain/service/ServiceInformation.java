2fe06e869d26a35fc758c0f14da7866049944aba
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-05-23 09:45:03 +0100

    CreateServiceInformationNextExceptionHandling

---
+++ src/main/java/com/example/ChatWithJMS/domain/service/ServiceInformation.java
@@ -0,0 +1,31 @@
+package com.example.ChatWithJMS.domain.service;
+
+import com.example.ChatWithJMS.domain.Information;
+import com.example.ChatWithJMS.domain.People;
+import com.example.ChatWithJMS.ports.DuctsRepo;
+import com.example.ChatWithJMS.ports.InformationRepo;
+import com.example.ChatWithJMS.ports.InformationService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.var;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@AllArgsConstructor(onConstructor_ = @Inject)
+@NoArgsConstructor
+public class ServiceInformation implements InformationService {
+
+    private InformationRepo informationRepo;
+    private DuctsRepo ductsRepo;
+
+    @Override
+    public void send(Information information, String ductName) {
+        var duct = ductsRepo.getDuctByName(ductName);
+        var peopleName = duct.get().getDuctPeople()
+                .stream()
+                .map(People::getPeopleName)
+                .toList();
+    }
+}
