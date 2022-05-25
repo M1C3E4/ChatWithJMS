6e668dd0bbef7a71a6752f83fdd6306fb9e4eaf5
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-05-25 11:56:19 +0100

    CreateMapperInShellApiNextCreateController

--- src/main/java/com/example/ChatWithJMS/adapters/api/mappers/DuctMapper.java
+++ src/main/java/com/example/ChatWithJMS/adapters/api/mappers/DuctMapper.java
@@ -1,4 +1,14 @@
 package com.example.ChatWithJMS.adapters.api.mappers;
 
+import com.example.ChatWithJMS.adapters.api.dataTransferObject.DuctDTO;
+import com.example.ChatWithJMS.domain.Duct;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import java.util.UUID;
+
+@Mapper(componentModel = "cdi", imports = {UUID.class})
 public interface DuctMapper {
+    DuctDTO toDto(Duct duct);
+    @Mapping(target = "ductId", expression = "java(java.util.UUID.randomUUID())")
+    Duct toDomain(DuctDTO ductDTO);
 }
